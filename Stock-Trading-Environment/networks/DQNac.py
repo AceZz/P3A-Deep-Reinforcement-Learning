89438cc54dc1f244fd17403daffa35591b31bb7b
tcarta <t.carta@hotmail.fr>
2020-02-04 01:38:37 +0100

    worked on DQNac

--- Stock-Trading-Environment/networks/DQNac.py
+++ Stock-Trading-Environment/networks/DQNac.py
@@ -18,6 +18,7 @@ class MyModel(tf.keras.Model):
         N = 6
         L = 6
         
+        self.enter=tf.keras.layers.InputLayer(input_shape= (1,6,6))
         self.conv1 = tf.keras.layers.Conv2D(
             filters = nbr_filters, kernel_size = kernel_size, strides = strides, padding = "valid"
             )
@@ -26,19 +27,19 @@ class MyModel(tf.keras.Model):
         self.norm=tf.keras.layers.BatchNormalization()
         
         self.conv2 = tf.keras.layers.Conv2D(
-            filters = 1, kernel_size = (1,2), strides = strides, padding = "valid" 
+            filters = 1, kernel_size = (1,2), strides = strides, padding = "valid",name=out 
             )      
          #kernel_initializer = tf.keras.initializers.TruncatedNormal([1,1,32,1], stddev=0.15)
             
-    @tf.function
-    def call(self, inputs):
-        z = inputs
-        z = self.conv1(z)
-        z = self.norm(z)
-        z = self.conv2(z)
-
-        
-        return z
+        self.model = tf.keras.Sequential()
+            
+    #@tf.function
+    def build_model(self):
+        self.model.add(self.enter)
+        self.model.add(self.conv1)
+        self.model.add(self.norm)
+        self.model.add(self.conv2)
+        return self.model
     
 class StockActor:
     #Initial hyperparaters
@@ -51,26 +52,30 @@ class StockActor:
         self.learning_rate = 10e-2
         self.gamma = 0.99
         
-        
-        self.model = MyModel(32, (1,4), (1,1))        
-        self.dense = tf.keras.layers.Dense(
+        mod = MyModel(32, (1,5), (1,1))
+        self.model = mod.build_model()        
+        self.dense1 = tf.keras.layers.Dense(
                 1, activation='tanh', kernel_initializer='RandomNormal')
-        
+        #self.dense2 = tf.keras.layers.Dense(
+         #       1, activation='tanh', kernel_initializer='RandomNormal')        
     
-    @tf.function
-    def build_actor(self,inputs):
-            z = inputs
-            z = self.model(z)
-            z = tf.squeeze(z)
-            z = tf.expand_dims(z, 0)
-            z = self.dense(z)
+    #@tf.function
+    def build_actor(self):
+        
+        self.model.add(self.dense1)
+        #self.model.add(self.dense2)    
+            #z = inputs
+            #z = self.model(z)
+            #z = tf.squeeze(z)
+            #z = tf.expand_dims(z, 0)
+            #z = self.dense(z)
 
-            return z
+        return self.model
         
         
-    def predict(self, inputs):
+    def predict(self,inputs):
         
-        return self.build_actor(inputs.astype('float32'))  
+        return self.model.predict(inputs.astype('float32'))  
     
         
 class StockCritic:
@@ -81,39 +86,58 @@ class StockCritic:
         #super(StockActor, self).__init__()
         
         self.tau = 10e-3
-        self.learning_rate = 10e-2
+        self.learning_rate = 10e-4
         self.gamma = 0.99
-        
-        self.model = MyModel(32, (1,4), (1,1))
+        self.action= tf.constant([[[[0.0]]]])
+
+        mod = MyModel(32, (1,4), (1,1))
+        self.model = mod.build_model()
+        self.model.get_layer()
+        #self.addi = tf.keras.layers.add([self.model,self.action])
+        #self.conca = tf.keras.layers.Concatenate([self.model,self.action])
         self.dense1 = tf.keras.layers.Dense(
             10, activation='tanh', kernel_initializer='RandomNormal')
         self.dense2 = tf.keras.layers.Dense(
             10, activation='tanh', kernel_initializer='RandomNormal')        
         self.dense3 = tf.keras.layers.Dense(
             1, activation='tanh', kernel_initializer='RandomNormal')
+        
+        self.optimizer = tf.optimizers.Adam(self.learning_rate)
+        
+        
     
     
-    @tf.function    
-    def build_critic(self,inputs,action):
-            z = inputs
-            z = self.model(z)
-            z = tf.squeeze(z)
-            a = [z[0], z[1], action[0][0]]
-            z = tf.convert_to_tensor(a)
-            z = tf.expand_dims(z, 0)
-            z = self.dense1(z)
-            z = self.dense2(z)
-            z = self.dense3(z)            
+    #@tf.function    
+    def build_critic(self):
+        #self.model.add(self.conca)
+        self.model.add(self.dense1)
+        self.model.add(self.dense2)
+        self.model.add(self.dense3)
+            
+            #z = inputs
+            #z = self.model(z)
+            #z = tf.squeeze(z)
+            #a = [z[0], z[1], action[0][0]]
+            #z = tf.convert_to_tensor(a)
+            #z = tf.expand_dims(z, 0)
+            #z = self.dense1(z)
+            #z = self.dense2(z)
+            #z = self.dense3(z)            
 
-            return z       
+        return self.model       
         
         
     def predict(self, inputs,action):
         
-        return self.build_critic(inputs.astype('float32'),action)      
+        self.action = action
+        
+        return self.model.predict(inputs.astype('float32'))
+    
+    def optimize(self):
+        return self.model.summary()#self.model.trainable_variables
 
         
-        
+            
         
         
         
